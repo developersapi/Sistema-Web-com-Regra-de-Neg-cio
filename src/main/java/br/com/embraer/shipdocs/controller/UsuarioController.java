e96d766b9cd37e1f14db00a75fdac58ce763b3eb
rafaeldossper <rafahsanttos96@gmail.com>
2021-09-16 21:55:56 -0300

    update usuario controller

--- src/main/java/br/com/embraer/shipdocs/controller/UsuarioController.java
+++ src/main/java/br/com/embraer/shipdocs/controller/UsuarioController.java
@@ -8,4 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/usuario")
 @RestController
 public class UsuarioController {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	@ResponseBody
+	@GetMapping(value = "/buscar")
+	public List<Usuario> buscarTodos() {
+		return usuarioRepository.findAll();
+
+	}
+
+	@ResponseBody
+	@GetMapping(value = "/buscar/{usuarioId}")
+	public Usuario buscarPorId(@PathVariable Long usuarioId) {
+		return usuarioRepository
+				.findById(usuarioId.toString())
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usu·rio n√£o encontrado"));
 }
